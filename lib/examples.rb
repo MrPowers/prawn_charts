9e59bb2be4cbd86fa5c74e01de5623c498146811
MrPowers <matthewkevinpowers@gmail.com>
2013-07-29 09:53:45 -0400

    Delete linear example

--- lib/examples.rb
+++ lib/examples.rb
@@ -1,20 +1,5 @@
 require_relative "prawn_charts"
 
-# linear example
-#input_data = [["Jan 11", 5], ["Feb 11", 77], ["Mar 11", 99], ["Apr 11", 105], ["May 11", 32], ["Jun 11", 1], ["Jul 11", 20]]
-#graph_height_pdf = 200
-#graph_width_pdf = 300
-#dot_radius = 4
-
-#pdf = ::Prawn::Document.new
-#pdf.bounding_box([0, pdf.cursor], :width => graph_width_pdf, :height => graph_height_pdf) do
-  #pdf.stroke_bounds
-  #CreatePdfDocument.new(pdf, input_data, graph_width_pdf, graph_height_pdf, dot_radius).run
-#end
-#pdf.render_file(Dir.home + "/desktop/linear_prawn_graph.pdf")
-
-
-
 # log example
 input_data = [["Feb 11", 900], ["Mar 11", 800_000], ["Apr 11", nil], ["May 11", 9_000_000], ["June 11", 40]]
 scale = :log
