d6afa14159cf4bd8a977edc5455caea97ac337cc
MrPowers <matthewkevinpowers@gmail.com>
2013-08-07 18:50:30 -0400

    Move requires to below Module definition

--- lib/prawn_charts.rb
+++ lib/prawn_charts.rb
@@ -1,8 +1,8 @@
 require 'prawn'
 require_relative "./prawn_charts/version"
 
-Dir["./lib/data_collectors/**/*.rb"].each {|file| require file }
-Dir["./lib/renderers/**/*.rb"].each {|file| require file }
-
 module PrawnCharts
 end
+
+Dir["./lib/data_collectors/**/*.rb"].each {|file| require file }
+Dir["./lib/renderers/**/*.rb"].each {|file| require file }
--- lib/prawn_charts/version.rb
+++ lib/prawn_charts/version.rb
@@ -1,3 +1,3 @@
 module PrawnCharts
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
