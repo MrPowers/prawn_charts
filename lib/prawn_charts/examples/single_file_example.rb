7ebd85b01b076961c33250e7b648d4a23dbd157e
MrPowers <matthewkevinpowers@gmail.com>
2013-10-08 09:52:02 -0400

    Finish up prelim refactoring and now need to create smart inputs

--- lib/prawn_charts/examples/single_file_example.rb
+++ lib/prawn_charts/examples/single_file_example.rb
@@ -4,7 +4,8 @@ Prawn::Document.extensions << PrawnCharts::PrawnChartRenderer
 
 pdf = Prawn::Document.new
 
+y_labels = [0, 5, 10, 15, 20]
 data = [{y: 5, x_label: "Jan 11"}, {y: 17, x_label: "Feb 11"}, {y: 14, x_label: "Mar 11"}]
-pdf.draw_graph([100, 200], 300, 400, data)
+pdf.draw_graph([100, 200], 300, 200, data, y_labels)
 
 pdf.render_file(Dir.home + "/desktop/simple_linear_prawn_graph.pdf")
