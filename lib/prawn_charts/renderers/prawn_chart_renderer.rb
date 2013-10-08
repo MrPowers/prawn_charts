17c60d355392737b04bd7ab36612597c3f2cf878
MrPowers <matthewkevinpowers@gmail.com>
2013-10-08 09:52:02 -0400

    Finish up prelim refactoring and now need to create smart inputs

--- lib/prawn_charts/renderers/prawn_chart_renderer.rb
+++ lib/prawn_charts/renderers/prawn_chart_renderer.rb
@@ -6,18 +6,71 @@ module PrawnCharts
       end
     end
 
-    def draw_dots(graph_points, dot_radius)
+    def draw_dots(graph_points, dot_radius = 4)
       graph_points.each do |point|
         fill_circle(point, dot_radius)
       end
     end
 
-    def draw_graph(point, width, height, data, dot_radius = 4)
-      graph_points = SingleFileDataCollector.new(height, width, data).graph_data_points
-      translate(*point) do
+    def draw_x_labels(label_data)
+      text_box_width = 100
+      horizontal_offset = text_box_width / 2
+      translate(0, -70) do
+        label_data.each do |data|
+         text_box(data[:label], {at: [(data[:x] - horizontal_offset), 20], width: text_box_width, align: :center, rotate: 45})
+        end
+      end
+    end
+
+    def draw_y_labels(label_data)
+      text_box_height = 40
+      vertical_offset = text_box_height / 2
+      translate(-40, 0) do
+        label_data.each do |data|
+          text_box(data[:label], {at: [5, data[:y] + vertical_offset], height: text_box_height, width: 40, valign: :center})
+        end
+      end
+    end
+
+    def draw_horizontal_lines(data)
+      data.each do |start_point, end_point|
+        stroke_line(start_point, end_point)
+      end
+    end
+
+    def draw_title(title, graph_height, graph_width, vertical_offset, text_box_options)
+      default_options = {at: [0, -20], width: graph_width, height: 20, align: :center}
+      translate(0, graph_height + vertical_offset) do
+        text_box(title, default_options.merge(text_box_options))
+      end
+    end
+
+    def draw_x_title(title, graph_width, vertical_offset, text_box_options)
+      default_options = {at: [0, 20], width: graph_width, height: 20, align: :center}
+      translate(0, vertical_offset) do
+        text_box(title, default_options.merge(text_box_options))
+      end
+    end
+
+    def draw_y_title(title, graph_height, horizontal_offset, text_box_options)
+      default_options = {at: [0, 0], width: graph_height, height: 20, align: :center, valign: :center, rotate: 90}
+      translate(horizontal_offset, 0) do
+        text_box(title, default_options.merge(text_box_options))
+      end
+    end
+
+    def draw_graph(input)
+      collector = SingleFileDataCollector.new(input.fetch(:graph))
+      translate(*starting_point) do
         stroke_rectangle([0, height], width, height)
-        draw_line(graph_points)
-        draw_dots(graph_points, dot_radius)
+        draw_line(collector.graph_data_points)
+        draw_dots(collector.graph_data_points)
+        draw_x_labels(collector.x_labels)
+        draw_y_labels(collector.y_labels)
+        draw_horizontal_lines(collector.horizontal_lines)
+        draw_title("Cool graph", collector.height, collector.width, 50, {})
+        draw_x_title("X Title", collector.width, -60, {})
+        draw_y_title("Y Title", collector.height, -50, {})
       end
     end
   end
--- spec/data_collectors/single_file_data_collector_spec.rb
+++ spec/data_collectors/single_file_data_collector_spec.rb
@@ -9,47 +9,10 @@ module PrawnCharts
       @collector = SingleFileDataCollector.new(height, width, data)
     end
 
-    context "#x_units" do
-      it "returns the number of x units" do
-        expect(@collector.x_units).to eq(1)
-      end
-    end
-
-    context "#pdf_points_per_x_unit" do
-      it "returns the number of pdf points per x unit" do
-        expect(@collector.pdf_points_per_x_unit).to eq(80)
-      end
-    end
-
-    context "#y_units" do
-      it "returns the number of y units" do
-        expect(@collector.y_units).to eq(17)
-      end
-    end
-
-    context "#pdf_points_per_y_unit" do
-      it "returns the number of pdf points per y unit" do
-        expect(@collector.pdf_points_per_y_unit).to be_within(0.01).of(3.53)
-      end
-    end
-
-    context "#x_pdf_data_points" do
-      it "returns an array of x pdf points" do
-        expected = [0, 80]
-        actual = @collector.x_pdf_data_points
-        actual.each_with_index do |actual_x_point, index|
-          expect(actual_x_point).to be_within(0.01).of expected[index]
-        end
-      end
-    end
-
-    context "#y_pdf_data_points" do
-      it "returns an array of y pdf points" do
-        expected = [17.65, 60]
-        actual = @collector.y_pdf_data_points
-        actual.each_with_index do |actual_y_point, index|
-          expect(actual_y_point).to be_within(0.01).of expected[index]
-        end
+    context "#x_labels_data" do
+      it "returns an array of x data points and labels" do
+        puts "hiiii"
+        puts @collector.x_labels_data
       end
     end
 
@@ -60,6 +23,50 @@ module PrawnCharts
         actual.each_with_index do |xy_point, index|
           expect(xy_point[0]).to be_within(0.01).of expected[index][0]
           expect(xy_point[1]).to be_within(0.01).of expected[index][1]
+        end
+      end
+    end
+
+    context "#x_units" do
+      it "returns the number of x units" do
+        expect(@collector.send(:x_units)).to eq(1)
+      end
+    end
+
+    context "#pdf_points_per_x_unit" do
+      it "returns the number of pdf points per x unit" do
+        expect(@collector.send(:pdf_points_per_x_unit)).to eq(80)
+      end
+    end
+
+    context "#y_units" do
+      it "returns the number of y units" do
+        expect(@collector.send(:y_units)).to eq(17)
+      end
+    end
+
+    context "#pdf_points_per_y_unit" do
+      it "returns the number of pdf points per y unit" do
+        expect(@collector.send(:pdf_points_per_y_unit)).to be_within(0.01).of(3.53)
+      end
+    end
+
+    context "#x_pdf_data_points" do
+      it "returns an array of x pdf points" do
+        expected = [0, 80]
+        actual = @collector.send(:x_pdf_data_points)
+        actual.each_with_index do |actual_x_point, index|
+          expect(actual_x_point).to be_within(0.01).of expected[index]
+        end
+      end
+    end
+
+    context "#y_pdf_data_points" do
+      it "returns an array of y pdf points" do
+        expected = [17.65, 60]
+        actual = @collector.send(:y_pdf_data_points)
+        actual.each_with_index do |actual_y_point, index|
+          expect(actual_y_point).to be_within(0.01).of expected[index]
         end
       end
     end
