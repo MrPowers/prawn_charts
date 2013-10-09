7511f9a86cba752f3a62aaa73438b73fc7a6c0b6
MrPowers <matthewkevinpowers@gmail.com>
2013-10-09 19:54:12 -0400

    Add RendererAssistant and refactor code so title options are configurable with smart defaults

--- lib/prawn_charts/examples/single_file_example.rb
+++ lib/prawn_charts/examples/single_file_example.rb
@@ -8,14 +8,13 @@ data = [{y: 5, x_label: "Jan 11"}, {y: 17, x_label: "Feb 11"}, {y: 14, x_label: 
 input = {
   :graph => {:starting_point => [100, 200], :width => 300, :height => 200, :data => data, :y_labels => [0, 5, 10, 15, 20]},
   :horizontal_lines => true,
-  :graph_title => { :title => "cool graph", :vertical_offset => 50, :text_box_options => {}},
-  :x_title => { :title => "x title", :vertical_offset => -60, :text_box_options => {}},
-  :y_title => { :title => "y title", :horizontal_offset => -50, :text_box_options => {}}
+  :graph_title => { :title => "cool graph", :offset => 50, :text_box_options => {} },
+  :x_title => { :title => "x title", :offset => -60, :text_box_options => {} },
+  :y_title => { :title => "y title", :offset => -50, :text_box_options => {} },
+  :x_labels => { :offset => -70, :text_box_options => { :width => 100, align: :center, rotate: 45 } },
+  :y_labels => { :offset => -40, :text_box_options => { :height => 40, valign: :center } }
 }
 
-
-#y_labels = [0, 5, 10, 15, 20]
-#data = [{y: 5, x_label: "Jan 11"}, {y: 17, x_label: "Feb 11"}, {y: 14, x_label: "Mar 11"}]
-pdf.draw_graph([100, 200], 300, 200, data, y_labels)
+pdf.draw_graph(input)
 
 pdf.render_file(Dir.home + "/desktop/simple_linear_prawn_graph.pdf")
--- lib/prawn_charts/renderers/prawn_chart_renderer.rb
+++ lib/prawn_charts/renderers/prawn_chart_renderer.rb
@@ -12,22 +12,22 @@ module PrawnCharts
       end
     end
 
-    def draw_x_labels(label_data)
-      text_box_width = 100
-      horizontal_offset = text_box_width / 2
-      translate(0, -70) do
+    def draw_x_labels(label_data, offset, options)
+      label_width = options[:width]
+      centering_adjustment = label_width / 2
+      translate(0, offset) do
         label_data.each do |data|
-         text_box(data[:label], {at: [(data[:x] - horizontal_offset), 20], width: text_box_width, align: :center, rotate: 45})
+         text_box(data[:label], {at: [(data[:x] - centering_adjustment), 20]}.merge(options))
         end
       end
     end
 
-    def draw_y_labels(label_data)
-      text_box_height = 40
-      vertical_offset = text_box_height / 2
-      translate(-40, 0) do
+    def draw_y_labels(label_data, offset, options)
+      label_height = options[:height]
+      centering_adjustment = label_height / 2
+      translate(offset, 0) do
         label_data.each do |data|
-          text_box(data[:label], {at: [5, data[:y] + vertical_offset], height: text_box_height, width: 40, valign: :center})
+          text_box(data[:label], {at: ([0, data[:y] + centering_adjustment]) }.merge(options))
         end
       end
     end
@@ -38,39 +38,25 @@ module PrawnCharts
       end
     end
 
-    def draw_title(title, graph_height, graph_width, vertical_offset, text_box_options)
-      default_options = {at: [0, -20], width: graph_width, height: 20, align: :center}
-      translate(0, graph_height + vertical_offset) do
-        text_box(title, default_options.merge(text_box_options))
-      end
-    end
-
-    def draw_x_title(title, graph_width, vertical_offset, text_box_options)
-      default_options = {at: [0, 20], width: graph_width, height: 20, align: :center}
-      translate(0, vertical_offset) do
-        text_box(title, default_options.merge(text_box_options))
-      end
-    end
-
-    def draw_y_title(title, graph_height, horizontal_offset, text_box_options)
-      default_options = {at: [0, 0], width: graph_height, height: 20, align: :center, valign: :center, rotate: 90}
-      translate(horizontal_offset, 0) do
-        text_box(title, default_options.merge(text_box_options))
+    def draw_title(translation, title, options)
+      translate(*translation) do
+        text_box(title, options)
       end
     end
 
     def draw_graph(input)
       collector = SingleFileDataCollector.new(input.fetch(:graph))
-      translate(*starting_point) do
-        stroke_rectangle([0, height], width, height)
+      assistant = RendererAssistant.new(input)
+      translate(*input[:graph][:starting_point]) do
+        stroke_rectangle([0, collector.height], collector.width, collector.height)
         draw_line(collector.graph_data_points)
         draw_dots(collector.graph_data_points)
-        draw_x_labels(collector.x_labels)
-        draw_y_labels(collector.y_labels)
-        draw_horizontal_lines(collector.horizontal_lines)
-        draw_title("Cool graph", collector.height, collector.width, 50, {})
-        draw_x_title("X Title", collector.width, -60, {})
-        draw_y_title("Y Title", collector.height, -50, {})
+        draw_x_labels(collector.x_labels, assistant.x_labels_offset, assistant.x_labels_options)
+        draw_y_labels(collector.y_labels, assistant.y_labels_offset, assistant.y_labels_options)
+        draw_horizontal_lines(collector.horizontal_lines) if input[:horizontal_lines]
+        draw_title(assistant.graph_title_translate, assistant.graph_title, assistant.graph_title_options) if input[:graph_title]
+        draw_title(assistant.x_title_translate, assistant.x_title, assistant.x_title_options) if input[:x_title]
+        draw_title(assistant.y_title_translate, assistant.y_title, assistant.y_title_options) if input[:y_title]
       end
     end
   end
